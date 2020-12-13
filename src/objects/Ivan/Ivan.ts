90ec9a6be26c83c2fb803753d9a8a7a50e2fdb6b
Kacper Pietrzak <kacpietrzak@gmail.com>
2020-12-13 07:13:34 +0100

    envs

--- src/objects/Ivan/Ivan.ts
+++ src/objects/Ivan/Ivan.ts
@@ -2,7 +2,7 @@ import { Inventory } from 'objects/Inventory';
 import { bound, EventEmitter } from 'packages/utils';
 import { Gun } from './Gun';
 
-const PLAYER_VELOCITY = 1200;
+const PLAYER_VELOCITY = process.env.BALANCING_MODE === 'true' ? 120 : 1200;
 export const PLAYER_MAX_HP = 100;
 
 type EventHandlers = {
